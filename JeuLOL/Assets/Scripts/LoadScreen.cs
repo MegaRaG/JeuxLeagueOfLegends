5403558c0838bb1bd3b169d6f5715cc302ba6953
Ahmed.O <omerovicahmed12@gmail.com>
2022-06-30 21:24:57 +0200

    ChangeScreen

---
+++ JeuLOL/Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour
+{
+ 
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    public void LoadJeu(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100 + "%";
+            yield return null;
+        }
+    }
+    public void doExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
