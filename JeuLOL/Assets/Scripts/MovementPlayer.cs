f6fb4acf7256c0bf2c0ac38d37b3188e81edc93a
Jereeem <jeremypell74@gmail.com>
2022-07-12 15:48:44 +0200

    character

---
+++ JeuLOL/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    public float speed;
+    public float climb;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounded;
+    [HideInInspector]
+    public bool isClimbing;
+
+    public Transform groundCheck;
+
+    public Rigidbody2D rigidBody;
+
+    public CapsuleCollider2D playerCollider;
+
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+    private float verticalMovement;
+
+   
+
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        verticalMovement = Input.GetAxis("Vertical") * climb * Time.fixedDeltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded && !isClimbing)
+        {
+            isJumping = true;
+        }
+
+    }
+
+   void FixedUpdate()
+    {
+        
+        MovePlayer(horizontalMovement, verticalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        if (!isClimbing)
+        {
+            Vector3 targetVelocity = new Vector2(_horizontalMovement, rigidBody.velocity.y);
+            rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, .05f);
+
+            if (isJumping)
+            {
+                rigidBody.AddForce(new Vector2(0f, jumpForce));
+                isJumping = false;
+            }
+        }
+        else
+        {
+            Vector3 targetVelocity = new Vector2(0, _verticalMovement);
+            rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, .05f);
+        }
+
+    }
+
+    
+
+}
