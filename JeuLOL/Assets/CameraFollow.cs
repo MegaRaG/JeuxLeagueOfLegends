eff083878241e6d5b366a771b8d780d0b607e700
Jereeem <jeremypell74@gmail.com>
2022-07-16 15:26:09 +0200

    camera

---
+++ JeuLOL/Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+
+    public float FollowSpeed = 2f;
+    public Transform target;
+    public float height = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + height , -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        
+    }
+}
