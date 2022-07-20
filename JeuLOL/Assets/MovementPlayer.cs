135d3c119d18c1423d738941511af1af1ac87344
Jereeem <jeremypell74@gmail.com>
2022-07-20 20:19:16 +0200

    poro

---
+++ JeuLOL/Assets/MovementPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+
+	public CharacterController controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+
+	void Update()
+	{
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+
+			crouch = true;
+		}
+		/*else if(Input.GetButtonUp("Crouch"))
+		{
+		crouch = false;
+
+		} */
+
+
+	}
+
+	void FixedUpdate()
+	{
+
+		//controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+
+	}
+
+}
